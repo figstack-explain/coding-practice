6569cdba3a573ea187544a4ab5deb22ab1e7ebba
Figstack <91431735+figstack-explain@users.noreply.github.com>
2021-09-27 16:53:37 -0400

    Make things tighter + remove comments

--- Quick_Sort.java
+++ Quick_Sort.java
@@ -2,44 +2,33 @@ import java.util.*;
 class Quick_Sort {
     
   public static int[] QuickSort(int[] arr, int elements) {
-      if(elements < 2){     //Base Case
-          return arr;
-      }
+      if(elements < 2){ return arr; }
 
-      int current_position=0;   //position of pivot element
-      int temp; //a temporary variable to assist in swapping
+      int current_position=0;
+      int temp;
 
-      for(int i=1; i<elements; i++) //Partitioning loop
-      {
-          if(arr[i] <= arr[0])
-          {
+      for(int i=1; i<elements; i++) {
+          if(arr[i] <= arr[0]) {
               current_position++;
               temp = arr[i];
               arr[i] = arr[current_position];
               arr[current_position] = temp;
-
           }
       }
 
       temp = arr[0]; 
       arr[0] = arr[current_position]; 
-      arr[current_position] = temp; //Brings pivot to it's appropriate position
-      
-      int[] left = QuickSort(arr,current_position); //sorts the elements to the left of pivot
-      
-      int[] arr2 = Arrays.copyOfRange(arr, current_position+1, elements);//separates elements right of pivot
-      
-      int[] right = QuickSort(arr2, elements-current_position-1); //sorts the elements to the right of pivot
-      
-      int[] final_array = new int[elements]; //final array, to merge everything together
-      
-      for(int i=0; i<current_position; i++)
-      {
+      arr[current_position] = temp; 
+      int[] left = QuickSort(arr,current_position);
+      int[] arr2 = Arrays.copyOfRange(arr, current_position+1, elements);
+      int[] right = QuickSort(arr2, elements-current_position-1);
+      int[] final_array = new int[elements];
+
+      for(int i=0; i<current_position; i++){
           final_array[i] = left[i]; 
       }
       final_array[current_position] = arr[current_position];
-      for(int i=current_position+1; i<elements; i++)
-      {
+      for(int i=current_position+1; i<elements; i++){
           final_array[i] = right[i-current_position-1];
       }
 
